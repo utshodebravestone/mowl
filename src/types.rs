53be87f9ffe8e54b9d58e8016a9fb7b9fa0e25b8
utsho <utshodebravestone@gmail.com>
2023-11-05 20:07:07 +0600

    added default functions

--- src/types.rs
+++ src/types.rs
@@ -10,6 +10,7 @@ pub enum AtomicExpression {
 pub enum Expression {
     Atom(AtomicExpression),
     List(Vec<Expression>),
+    Function(fn(&[Expression]) -> Result<Expression, Error>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -20,4 +21,77 @@ pub enum Error {
 #[derive(Debug)]
 pub struct Environment {
     pub bindings: HashMap<String, Expression>,
+}
+
+impl Default for Environment {
+    fn default() -> Self {
+        let bindings = HashMap::from([
+            (
+                "+".to_string(),
+                Expression::Function(|args| {
+                    if args.len() == 2 {
+                        if let Expression::Atom(AtomicExpression::Number(x)) = args[1] {
+                            if let Expression::Atom(AtomicExpression::Number(y)) = args[2] {
+                                return Ok(Expression::Atom(AtomicExpression::Number(x + y)));
+                            }
+                        }
+                    }
+                    Err(Error::Reason(
+                        "`+` only accepts numbers as it's argument".to_string(),
+                    ))
+                }),
+            ),
+            (
+                "-".to_string(),
+                Expression::Function(|args| {
+                    if args.len() == 2 {
+                        if let Expression::Atom(AtomicExpression::Number(x)) = args[1] {
+                            if let Expression::Atom(AtomicExpression::Number(y)) = args[2] {
+                                return Ok(Expression::Atom(AtomicExpression::Number(x - y)));
+                            }
+                        }
+                    }
+                    Err(Error::Reason(
+                        "`-` only accepts numbers as it's argument".to_string(),
+                    ))
+                }),
+            ),
+            (
+                "*".to_string(),
+                Expression::Function(|args| {
+                    if args.len() == 2 {
+                        if let Expression::Atom(AtomicExpression::Number(x)) = args[1] {
+                            if let Expression::Atom(AtomicExpression::Number(y)) = args[2] {
+                                return Ok(Expression::Atom(AtomicExpression::Number(x * y)));
+                            }
+                        }
+                    }
+                    Err(Error::Reason(
+                        "`*` only accepts numbers as it's argument".to_string(),
+                    ))
+                }),
+            ),
+            (
+                "/".to_string(),
+                Expression::Function(|args| {
+                    if args.len() == 2 {
+                        if let Expression::Atom(AtomicExpression::Number(x)) = args[1] {
+                            if let Expression::Atom(AtomicExpression::Number(y)) = args[2] {
+                                if y == 0. {
+                                    return Err(Error::Reason("can't divide by 0".to_string()));
+                                } else {
+                                    return Ok(Expression::Atom(AtomicExpression::Number(x / y)));
+                                }
+                            }
+                        }
+                    }
+                    Err(Error::Reason(
+                        "`/` only accepts numbers as it's argument".to_string(),
+                    ))
+                }),
+            ),
+        ]);
+
+        Self { bindings }
+    }
 }
