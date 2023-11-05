83369b0e0b923329281df08cb4a39f2b3b1ed91f
utsho <utshodebravestone@gmail.com>
2023-11-05 18:22:44 +0600

    added types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+enum Atom {
+    Symbol(String),
+    Number(f64),
+}
+
+#[derive(Debug)]
+enum Expression {
+    Atom(Atom),
+    List(Vec<Expression>),
+}
+
+#[derive(Debug)]
+enum Error {
+    Reason(String),
+}
+
+#[derive(Debug)]
+struct Environment {
+    bindings: HashMap<String, Expression>,
+}
+
 fn main() {
     println!("Hello, My Own Lisp!");
 }
